d012bb2cc473a1f05ab00462f771b993c08eea26
FintanH <fintan.halpenny@gmail.com>
2018-10-04 11:13:29 +0100

    ExplicitForall. coerce underlying function

--- containers-int-strict/src/Map/Int.hs
+++ containers-int-strict/src/Map/Int.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE ConstraintKinds            #-}
 {-# LANGUAGE DerivingStrategies         #-}
+{-# LANGUAGE ExplicitForAll             #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE RoleAnnotations            #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
 {-# LANGUAGE TypeApplications           #-}
@@ -47,50 +47,50 @@ type Key = (~) Int
 empty :: forall k v. Map k v
 empty = coerce @(M.IntMap v) M.empty
 
-singleton :: Key k => k -> v -> Map k v
-singleton k = coerce . M.singleton k
+singleton :: forall k v. Key k => k -> v -> Map k v
+singleton = coerce @(k -> v -> M.IntMap v) M.singleton
 
-fromList :: Key k => [(k, v)] -> Map k v
-fromList = coerce . M.fromList
+fromList :: forall k v. Key k => [(k, v)] -> Map k v
+fromList = coerce @([(k, v)] -> M.IntMap v) M.fromList
 
 null :: forall k v. Map k v -> Bool
-null = M.null . coerce @_ @(M.IntMap v)
+null = coerce @(M.IntMap v -> Bool) M.null
 
 size :: forall k v. Map k v -> Int
-size = M.size . coerce @_ @(M.IntMap v)
+size = coerce @(M.IntMap v -> Int) M.size
 
 member :: forall k v. Key k => k -> Map k v -> Bool
-member k = M.member k . coerce @_ @(M.IntMap v)
+member = coerce @(k -> M.IntMap v -> Bool) M.member
 
-lookup :: Key k => k -> Map k v -> Maybe v
-lookup k = M.lookup k . coerce
+lookup :: forall k v. Key k => k -> Map k v -> Maybe v
+lookup = coerce @(k -> M.IntMap v -> Maybe v) M.lookup
 
-lookupDefault :: Key k => v -> k -> Map k v -> v
-lookupDefault def k = M.findWithDefault def k . coerce
+lookupDefault :: forall k v. Key k => v -> k -> Map k v -> v
+lookupDefault = coerce @(v -> k -> M.IntMap v -> v) M.findWithDefault
 
-toList :: Key k => Map k v -> [(k, v)]
-toList = M.toList . coerce
+toList :: forall k v. Key k => Map k v -> [(k, v)]
+toList = coerce @(M.IntMap v -> [(k, v)]) M.toList
 
 keys :: forall k v. Key k => Map k v -> [k]
-keys = M.keys . coerce @_ @(M.IntMap v)
+keys = coerce @(M.IntMap v -> [k]) M.keys
 
-elems :: Map k v -> [v]
-elems = M.elems . coerce
+elems :: forall k v. Map k v -> [v]
+elems = coerce @(M.IntMap v -> [v]) M.elems
 
-insert :: Key k => k -> v -> Map k v -> Map k v
-insert k v = coerce . M.insert k v . coerce
+insert :: forall k v. Key k => k -> v -> Map k v -> Map k v
+insert = coerce @(k -> v -> M.IntMap v -> M.IntMap v) M.insert
 
-insertWith :: Key k => (v -> v -> v) -> k -> v -> Map k v -> Map k v
-insertWith f k v= coerce . M.insertWith f k v . coerce
+insertWith :: forall k v. Key k => (v -> v -> v) -> k -> v -> Map k v -> Map k v
+insertWith = coerce @((v -> v -> v) -> k -> v -> M.IntMap v -> M.IntMap v) M.insertWith
 
-adjust :: Key k => (a -> a) -> k -> Map k a -> Map k a
-adjust f k = coerce . M.adjust f k . coerce
+adjust :: forall k v. Key k => (v -> v) -> k -> Map k v -> Map k v
+adjust = coerce @((v -> v) -> k -> M.IntMap v -> M.IntMap v) M.adjust
 
-update :: Key k => (a -> Maybe a) -> k -> Map k a -> Map k a
-update f k = coerce . M.update f k . coerce
+update :: forall k v. Key k => (v -> Maybe v) -> k -> Map k v -> Map k v
+update = coerce @((v -> Maybe v) -> k -> M.IntMap v -> M.IntMap v) M.update
 
 delete :: forall k v. Key k => k -> Map k v -> Map k v
-delete k = coerce . M.delete k . coerce @_ @(M.IntMap v)
+delete = coerce @(k -> M.IntMap v -> M.IntMap v) M.delete
 
-alter :: Key k => (Maybe v -> Maybe v) -> k -> Map k v -> Map k v
-alter f k = coerce . M.alter f k . coerce
+alter :: forall k v. Key k => (Maybe v -> Maybe v) -> k -> Map k v -> Map k v
+alter = coerce @((Maybe v -> Maybe v) -> k -> M.IntMap v -> M.IntMap v) M.alter
