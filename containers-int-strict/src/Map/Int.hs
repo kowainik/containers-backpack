a62a57b8f9b2b72570aace4bc8290a58dfb8fb4d
FintanH <fintan.halpenny@gmail.com>
2018-10-04 10:34:03 +0100

    Use TypeApplications, ScopedTypeVariables and RankNTypes to coerce

--- containers-int-strict/src/Map/Int.hs
+++ containers-int-strict/src/Map/Int.hs
@@ -2,7 +2,10 @@
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE RankNTypes                 #-}
 {-# LANGUAGE RoleAnnotations            #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeApplications           #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Map.Int
@@ -32,61 +35,62 @@ module Map.Int
        ) where
 
 import Prelude hiding (lookup, null)
+import Data.Coerce (coerce)
 
 import qualified Data.IntMap.Strict as M
 
-newtype Map k v = IM { unIM :: M.IntMap v }
+newtype Map k v = IM (M.IntMap v)
     deriving newtype (Show, Eq)
 
 type Key = (~) Int
 
-empty :: Map k v
-empty = IM M.empty
+empty :: forall k v. Map k v
+empty = coerce @(M.IntMap v) M.empty
 
 singleton :: Key k => k -> v -> Map k v
-singleton k = IM . M.singleton k
+singleton k = coerce . M.singleton k
 
 fromList :: Key k => [(k, v)] -> Map k v
-fromList = IM . M.fromList
+fromList = coerce . M.fromList
 
-null :: Map k v -> Bool
-null = M.null . unIM
+null :: forall k v. Map k v -> Bool
+null = M.null . coerce @_ @(M.IntMap v)
 
-size :: Map k v -> Int
-size = M.size . unIM
+size :: forall k v. Map k v -> Int
+size = M.size . coerce @_ @(M.IntMap v)
 
-member :: Key k => k -> Map k a -> Bool
-member k = M.member k . unIM
+member :: forall k v. Key k => k -> Map k v -> Bool
+member k = M.member k . coerce @_ @(M.IntMap v)
 
 lookup :: Key k => k -> Map k v -> Maybe v
-lookup k = M.lookup k . unIM
+lookup k = M.lookup k . coerce
 
 lookupDefault :: Key k => v -> k -> Map k v -> v
-lookupDefault def k = M.findWithDefault def k . unIM
+lookupDefault def k = M.findWithDefault def k . coerce
 
 toList :: Key k => Map k v -> [(k, v)]
-toList = M.toList . unIM
+toList = M.toList . coerce
 
-keys :: Key k => Map k v -> [k]
-keys = M.keys . unIM
+keys :: forall k v. Key k => Map k v -> [k]
+keys = M.keys . coerce @_ @(M.IntMap v)
 
 elems :: Map k v -> [v]
-elems = M.elems . unIM
+elems = M.elems . coerce
 
 insert :: Key k => k -> v -> Map k v -> Map k v
-insert k v = IM . M.insert k v . unIM
+insert k v = coerce . M.insert k v . coerce
 
 insertWith :: Key k => (v -> v -> v) -> k -> v -> Map k v -> Map k v
-insertWith f k v= IM . M.insertWith f k v . unIM
+insertWith f k v= coerce . M.insertWith f k v . coerce
 
 adjust :: Key k => (a -> a) -> k -> Map k a -> Map k a
-adjust f k = IM . M.adjust f k . unIM
+adjust f k = coerce . M.adjust f k . coerce
 
 update :: Key k => (a -> Maybe a) -> k -> Map k a -> Map k a
-update f k = IM . M.update f k . unIM
+update f k = coerce . M.update f k . coerce
 
-delete :: Key k => k -> Map k v -> Map k v
-delete k = IM . M.delete k . unIM
+delete :: forall k v. Key k => k -> Map k v -> Map k v
+delete k = coerce . M.delete k . coerce @_ @(M.IntMap v)
 
 alter :: Key k => (Maybe v -> Maybe v) -> k -> Map k v -> Map k v
-alter f k = IM . M.alter f k . unIM
+alter f k = coerce . M.alter f k . coerce
