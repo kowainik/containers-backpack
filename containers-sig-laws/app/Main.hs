a6110ddde03ba2d9465c7c2db6158cc45d084301
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2018-10-21 15:40:04 +0300

    Use aliases for K-V types in sig-laws and use short texts

--- containers-sig-laws/app/Main.hs
+++ containers-sig-laws/app/Main.hs
@@ -1,29 +1,47 @@
+{-# LANGUAGE DeriveAnyClass   #-}
+{-# LANGUAGE DeriveGeneric    #-}
 {-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import qualified Map.Contrib.Laws.RO.Prim as ROPR
-import qualified Map.Contrib.Laws.Int as LI
-import qualified Map.Contrib.Laws.RO.Int as ROLI
-import qualified Map.Contrib.Laws.Ord as LO
-import qualified Map.Contrib.Laws.RO.Ord as ROLO
+import Data.Hashable (Hashable)
+import Data.Proxy (Proxy (Proxy))
+import qualified Data.Text as T
+import GHC.Generics (Generic)
 import qualified Map.Contrib.Laws.Hash as LH
+import qualified Map.Contrib.Laws.Int as LI
+import qualified Map.Contrib.Laws.Ord as LO
 import qualified Map.Contrib.Laws.RO.Hash as ROLH
-import Data.Proxy
+import qualified Map.Contrib.Laws.RO.Int as ROLI
+import qualified Map.Contrib.Laws.RO.Ord as ROLO
+import qualified Map.Contrib.Laws.RO.Prim as ROPR
+import Test.QuickCheck (Arbitrary, arbitrary, choose, vectorOf)
+
+-- | ShortText is a Text with length [0, 3] chosen "arbitrarily"
+data ShortText = ShortText T.Text
+  deriving (Show, Eq, Ord, Generic, Hashable)
+
+-- | Generates a Text with length [0, 3]. Choosing a small text speeds up the quickCheck tests.
+-- Note: do not make minimum larger than 0, empty texts are often edge cases.
+instance Arbitrary ShortText where
+  arbitrary = ShortText . T.pack <$> (choose (0, 3) >>= flip vectorOf arbitrary)
+
+type K = Int
+type V = ShortText
 
 main :: IO ()
 main = do
   -- * Laws for Map.Prim implementation
-  ROPR.checkLaws (Proxy @String) (Proxy @String)
+  ROPR.checkLaws (Proxy @K) (Proxy @V)
 
-  -- * Laws for Map.Int implementation
-  LI.checkLaws (Proxy @Int) (Proxy @String)
-  ROLI.checkLaws (Proxy @Int) (Proxy @String)
+  -- * Laws for Map.T.Text implementation
+  LI.checkLaws (Proxy @Int) (Proxy @V)
+  ROLI.checkLaws (Proxy @Int) (Proxy @V)
 
   -- * Laws for Map.Ord implementation
-  LO.checkLaws (Proxy @Int) (Proxy @String)
-  ROLO.checkLaws (Proxy @Int) (Proxy @String)
+  LO.checkLaws (Proxy @K) (Proxy @V)
+  ROLO.checkLaws (Proxy @K) (Proxy @V)
 
   -- * Laws for Map.Hash implementation
-  LH.checkLaws (Proxy @Int) (Proxy @String)
-  ROLH.checkLaws (Proxy @Int) (Proxy @String)
+  LH.checkLaws (Proxy @K) (Proxy @V)
+  ROLH.checkLaws (Proxy @K) (Proxy @V)
