a1ccb7f01db707b48e523e4c559046e5c9d3b329
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-10-16 09:11:38 +0300

    Fix after review

--- test/Main.hs
+++ test/Main.hs
@@ -6,7 +6,7 @@ import Data.Hashable (Hashable)
 import Data.Proxy (Proxy (Proxy))
 import Data.Text (Text)
 import GHC.Generics (Generic)
-import Test.QuickCheck (Arbitrary, arbitrary, choose, vectorOf)
+import Test.QuickCheck (Arbitrary, Gen, arbitrary, choose, vectorOf)
 
 import qualified Data.Text as T
 import qualified Map.Laws.Hash as Hash
@@ -15,9 +15,10 @@ import qualified Map.Laws.Ord as Ord
 
 
 -- | ShortText is a Text with length [0, 3] chosen "arbitrarily"
-newtype ShortText = ShortText Text
-    deriving stock (Show, Generic)
-    deriving newtype (Eq, Ord, Hashable)
+newtype ShortText = ShortText
+    { unShortText :: Text
+    } deriving stock (Show, Generic)
+      deriving newtype (Eq, Ord, Hashable)
 
 {- | Generates a Text with length [0, 3]. Choosing a small text speeds up the
 quickCheck tests.
@@ -25,6 +26,7 @@ quickCheck tests.
 Note: do not make minimum larger than 0, empty texts are often edge cases.
 -}
 instance Arbitrary ShortText where
+    arbitrary :: Gen ShortText
     arbitrary = ShortText . T.pack <$> (choose (0, 3) >>= flip vectorOf arbitrary)
 
 type K = Int
@@ -32,7 +34,7 @@ type V = ShortText
 
 main :: IO ()
 main = do
-  Int.checkLaws (Proxy @Int) (Proxy @V)
-  Ord.checkLaws (Proxy @K) (Proxy @V)
-  Hash.checkLaws (Proxy @K) (Proxy @V)
-  -- Prim.checkLaws (Proxy @K) (Proxy @V)
+    Int.checkLaws (Proxy @Int) (Proxy @V)
+    Ord.checkLaws (Proxy @K) (Proxy @V)
+    Hash.checkLaws (Proxy @K) (Proxy @V)
+    -- Prim.checkLaws (Proxy @K) (Proxy @V)
